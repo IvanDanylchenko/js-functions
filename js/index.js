8cf398a1a648a2d7828a9eaa547ed20d1680274d
IvanDanylchenko <danylchenko21@gmail.com>
2022-01-17 21:06:07 +0200

    Add task 1 & 2

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ console.log(isWorkingAgePerson(88));
 
 /**
  * Function checks working age
- * @param {Number} age 
+ * @param {Number} age
  * @returns {boolean} true or false
  */
 
@@ -19,5 +19,38 @@ console.groupEnd();
 
 console.group("Task 1");
 
+// console.log(primeNumber(n));
+
+const n = Number(prompt("Input number"));
+console.log(primeNumber(n));
+
+/**
+ * Function checks prime number or not
+ * @param {Number} n
+ * @returns {boolean} true or false
+ */
+
+function primeNumber(n) {
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) return false;
+  }
+  return n > 1;
+}
+
 console.groupEnd();
 
+console.group("Task 2");
+
+function checkMultiplicity(n1, n2) {
+  if (Number.isInteger(n1 / n2)) {
+    return true;
+  }
+  return false;
+}
+
+console.log(checkMultiplicity(25, 5));
+console.log(checkMultiplicity(15, 3));
+console.log(checkMultiplicity(15, 5));
+console.log(checkMultiplicity(15, 4));
+
+console.groupEnd();
